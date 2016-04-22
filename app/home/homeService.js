f9101fe3adec92ef6bba007a062972c750be3335
Hung Nguyen <hungngit@gmail.com>
2016-04-22 18:30:55 +0700

    yes

--- app/home/homeService.js
+++ app/home/homeService.js
@@ -2,16 +2,94 @@
 	'use strict';
 	app.factory('homeService', homeService);
 
-	homeService.$inject = ['apiService'];
+	homeService.$inject = ['$q', 'apiService'];
 
-	function homeService(apiService) {
+	function homeService($q, apiService) {
 		var config = {headers: {'X-Redmine-API-Key' : '58e93bc79d51bd7052a09ed5bc8597e851fb2e6d'}};
 		var homeService = {
 			getTimeEntries: getTimeEntries
 		}
 		
+		var spentOn = 't';
+		var timeEntriesUrl = 'http://14.161.22.172:3000/time_entries.json?f[]=spent_on&op[spent_on]=' + spentOn + '&f[]=user_id&op[user_id]==&v[user_id][]=56&v[user_id][]=54&v[user_id][]=35&v[user_id][]=25&v[user_id][]=38&v[user_id][]=12&v[user_id][]=34&v[user_id][]=23&f[]=&c[]=project&c[]=spent_on&c[]=user&c[]=activity&c[]=issue&c[]=comments&c[]=hours';
+		var issueUrl = 'http://14.161.22.172:3000/issues/{0}.json'
 		function getTimeEntries() {
-			apiService.get('http://14.161.22.172:3000/time_entries.json?f[]=spent_on&op[spent_on]=t&f[]=user_id&op[user_id]==&v[user_id][]=56&v[user_id][]=54&v[user_id][]=35&v[user_id][]=25&v[user_id][]=38&v[user_id][]=12&v[user_id][]=34&v[user_id][]=23&f[]=&c[]=project&c[]=spent_on&c[]=user&c[]=activity&c[]=issue&c[]=comments&c[]=hours', getTimeEntriesData, null);
+			apiService.get(timeEntriesUrl).then(function (data){
+				
+				var time_entries = data.time_entries;
+
+				console.log(getSpentTimeAll(time_entries));
+			});
+		}
+
+		function getSpentTimeAll(time_entries){
+			//var spentTimeAll = {};
+			var deferred = $q.defer();
+			var promises = [];
+
+			angular.forEach(time_entries, function(timeEntry, i){
+				promises.push(getIssueInfo(timeEntry, i));
+			});
+			$q.all(promises).then(
+				function(timeEntry, i) {
+					deferred.resolve(timeEntry, i) ;
+				},
+				function(errors) {
+					deferred.reject(errors);
+				},
+				function(updates) {
+					deferred.update(updates);
+			});
+			return deferred.promise;
+		}
+
+		function getIssueInfo(timeEntry, _i){
+
+			console.log('----------------- ' + _i + ' ******');
+			// Creates a Deferred object
+			var deferred = $q.defer();
+			apiService.get(issueUrl.replace('{0}', timeEntry.issue.id)).then(function(data){
+				var issue = data.issue;
+				var ksLink = '';
+				
+				if ('CodingPhase' == issue.tracker.name){
+					timeEntry.issue.id = issue.parent.id;
+					console.log('----------------- ' + _i + ' CodingPhase');
+					return getIssueInfo(timeEntry, _i);
+				}
+
+				if('Change' == issue.tracker.name ||
+					'Task' == issue.tracker.name){
+					console.log('----------------- ' + _i + ' Task');
+					angular.forEach(issue.custom_fields, function(field, i){
+						if ('KS_Link' == field.name){
+							ksLink = field.value;
+							if (ksLink.indexOf('a02') < 0 && 'Task' != issue.tracker.name){
+								timeEntry.issue.id = issue.parent.id;
+								console.log('----------------- ' + _i + ' Change');
+								return getIssueInfo(timeEntry, _i);
+							}
+						}
+					});
+				}
+
+				if ('★★Research&Training★★' == issue.tracker.name){
+					ksLink = 'https://ap.salesforce.com/a021000001824AJ';
+				}
+
+				var spentTime = {
+					issueId: timeEntry.issue.id,
+					issueName: issue.subject,
+					ksLink: ksLink,
+					DevName: timeEntry.user.name,
+					activity: timeEntry.activity.name,
+					hours: timeEntry.hours,
+					date: timeEntry.spent_on
+				};
+				console.log('----------------- ' + _i + ' ########');
+				deferred.resolve(spentTime);
+				return deferred.promise;
+			});
 		}
 
 		function getTimeEntriesData(result) {
@@ -28,7 +106,6 @@
 					hours: data.hours,
 					date: data.spent_on
 				};
-				console.table(data.user.name);
 			});
 		}
 
--- app/shared/service/apiService.js
+++ app/shared/service/apiService.js
@@ -3,47 +3,37 @@
 
 	app.factory('apiService', apiService);
 
-	apiService.$inject = ['$http'];
+	apiService.$inject = ['$http', '$q'];
 	
 	/**
 	 * Represents a Api Service.
 	 * Helps to handle all api call
 	 */
-	function apiService($http) {
+	function apiService($http, $q) {
 		var service = {
 			get: get,
 			post: post
 		};
 		var config = {headers: {'X-Redmine-API-Key' : '58e93bc79d51bd7052a09ed5bc8597e851fb2e6d'}};
-		var proxyUrl = '//nodejsproxy.herokuapp.com//proxy?url=';
+		var proxyUrl = '//nodejsproxy.herokuapp.com/proxy?url=';
 		// Handle all get requests
-		function get(url, success, failure) {
-			return $http.get(proxyUrl + encodeURI(url), config)
+		function get(url) {
+			url = proxyUrl + encodeURI(url);
+			return $q(function (success, failure) {
+				$http.get(url, config)
 					.then(function (result) {
-						success(result);
-					}, function (error) {
-						
-						if (error.status == '404') {
-							//notificationService.displayError(messageService.ApiServiceNotFound);
-						} else if (failure != null) {
-							failure(error);
-						}
-					});
+						success && success(result.data);
+					}, failure);
+			});
 		}
 		
 		// Handle all post requests
-		function post(url, data, success, failure) {
-			return $http.post(proxyUrl + encodeURI(url), data)
-					.then(function (result) {
-						//success(result);
-					}, function (error) {
-						if (error.status == '404') {
-							//notificationService.displayError(messageService.ApiServiceNotFound);
-						}
-						else if (failure != null) {
-							//failure(error);
-						}
-					});
+		function post(url, data) {
+			url = proxyUrl + encodeURI(url);
+			return $q(function (success, failure) {
+				$http.post(url, data, config)
+					.then(success, failure);
+			});
 		}
 
 		return service;
