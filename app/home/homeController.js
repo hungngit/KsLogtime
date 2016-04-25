f1420cf8d8a18d0b4d11240fa8a8ba4a5fd88ed0
Hung Nguyen <hungngit@gmail.com>
2016-04-25 18:55:28 +0700

    aaaa

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -2,12 +2,20 @@
 	'use strict';
 
 	app.controller('homeController', homeController);
-	homeController.$inject = ['$scope', '$q', 'homeService'];
+	homeController.$inject = ['$scope', 'homeService'];
 	
-	function homeController($scope, $q, homeService) {
-		this.test = 'Test from controller';
+	function homeController($scope, homeService) {
+		//this.test = 'Test from controller';
 		homeService.getTimeEntries().then(function (timeEntries){
 			console.log(timeEntries);
+
+			/*timeEntries = timeEntries.sort(function(a, b){
+				a = parseInt(a.parentIssueId ? a.parentIssueId : 0);
+				b = parseInt(b.parentIssueId ? b.parentIssueId : 0);
+				return b - a;
+			});
+*/
+			$scope.timeEntries = timeEntries;
 		});
 	}
 
--- app/home/homeService.js
+++ app/home/homeService.js
@@ -10,14 +10,33 @@
 			getTimeEntries: getTimeEntries
 		}
 		
-		var spentOn = 'm';
-		var timeEntriesUrl = 'http://14.161.22.172:3000/time_entries.json?f[]=spent_on&op[spent_on]=' + spentOn + '&f[]=user_id&op[user_id]==&v[user_id][]=56&v[user_id][]=54&v[user_id][]=35&v[user_id][]=25&v[user_id][]=38&v[user_id][]=12&v[user_id][]=34&v[user_id][]=23&f[]=&c[]=project&c[]=spent_on&c[]=user&c[]=activity&c[]=issue&c[]=comments&c[]=hours';
+		var spentOn = 't';
+		var timeEntriesUrl = 'http://14.161.22.172:3000/time_entries.json?f[]=spent_on&op[spent_on]=' + spentOn + '&f[]=user_id&op[user_id]==&v[user_id][]=56&v[user_id][]=60&v[user_id][]=54&v[user_id][]=35&v[user_id][]=25&v[user_id][]=38&v[user_id][]=12&v[user_id][]=34&v[user_id][]=23&f[]=&c[]=project&c[]=spent_on&c[]=user&c[]=activity&c[]=issue&c[]=comments&c[]=hours';
 		var issueUrl = 'http://14.161.22.172:3000/issues/{0}.json'
 		
 		function getTimeEntries() {
 			var d = $q.defer();
 			apiService.get(timeEntriesUrl).then(function (data){
 				getSpentTimeAll(data.time_entries).then(function (timeEntries){
+					// Sort and group --- Start
+					/*var result = timeEntries.reduce(function(timeEntryMap, timeEntry) {
+						//console.log(timeEntry);
+						var key = timeEntry.childIssue + '-' + timeEntry.ksLink + '-' + timeEntry.date + '-' + timeEntry.devId;
+						//console.log('-----------------');
+						//console.log(key);
+						if (!(key in timeEntryMap))
+							timeEntryMap.__array.push(timeEntryMap[key] = timeEntry);
+						else {
+							timeEntryMap[key].hours += timeEntry.hours;
+							//console.log(key);
+						}
+						//console.log(timeEntryMap);
+						return timeEntryMap;
+					}, {__array:[]}).__array.sort(function(a,b) { 
+						return a.devId - b.devId; 
+					});*/
+					// Sort --- End
+
 					d.resolve(timeEntries);
 				});
 			});
@@ -35,24 +54,25 @@
 
 		function getIssueInfo(timeEntry){
 			var d = $q.defer();
-			
+			var childIssue = [];
 			(function loadIssue(id){
 				apiService.get(issueUrl.replace('{0}', id)).then(function(data){
-					var issue = data.issue;
-					var ksLink = '';					
+					var issue = data.issue, 
+						ksLink = '';
+					childIssue.push({issueId: issue.id, issueName: issue.subject});
 					if ('CodingPhase' == issue.tracker.name){
-						timeEntry.issue.id = issue.parent.id;
-						return loadIssue(timeEntry.issue.id);
+						return loadIssue(issue.parent.id);
 					}
 
-					if('Change' == issue.tracker.name ||
-						'Task' == issue.tracker.name){
+					if('Task' == issue.tracker.name ||
+						'Change' == issue.tracker.name ||
+						'Bug' == issue.tracker.name){
 						angular.forEach(issue.custom_fields, function(field, i){
 							if ('KS_Link' == field.name){
 								ksLink = field.value;
-								if (ksLink.indexOf('a02') < 0 && 'Task' != issue.tracker.name){
-									timeEntry.issue.id = issue.parent.id;
-									return loadIssue(timeEntry.issue.id);
+								if (ksLink.indexOf('/a02') < 0 && 'Task' != issue.tracker.name){
+									console.log(issue.parent.id);
+									return loadIssue(issue.parent.id);
 								}
 							}
 						});
@@ -61,7 +81,7 @@
 					if ('★★Research&Training★★' == issue.tracker.name){
 						ksLink = 'https://ap.salesforce.com/a021000001824AJ';
 					}
-
+					console.log(ksLink);
 					var spentTime = {
 						issueId: timeEntry.issue.id,
 						issueName: issue.subject,
@@ -70,7 +90,8 @@
 						devName: timeEntry.user.name,
 						activity: timeEntry.activity.name,
 						hours: timeEntry.hours,
-						date: timeEntry.spent_on
+						date: timeEntry.spent_on,
+						childIssue: childIssue
 					};
 					d.resolve(spentTime);
 					
