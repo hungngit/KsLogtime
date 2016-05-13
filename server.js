f8292db50574c35639df56ac98137ec4605dc89a
Hung Nguyen <hungngit@gmail.com>
2016-05-13 14:53:47 +0700

    test

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var express = require('express');
 
 // Node express server setup.
 var server = express();
+
+server.use("/spentOn*", function(req, res, next){
+	res.sendFile(__dirname + '/index.html');
+});
+
+server.use("/super*", function(req, res, next){
+	res.sendFile(__dirname + '/index.html');
+});
+
 server.use(express.static(__dirname + '/'));
 
 server.set('port', (process.env.PORT || 80));
