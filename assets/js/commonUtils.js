000cfd18c23671ccc3032fe98ec4ad6736af41c1
Hung Nguyen <hungngit@gmail.com>
2016-06-16 10:33:44 +0700

    new gif

--- assets/js/commonUtils.js
+++ assets/js/commonUtils.js
@@ -5,15 +5,15 @@ var commonUtils = (function($){
 	}
 
 	function blockUI(){
-		var items = [1,2,3,4];
+		var items = [1,2,3,4,5,6];
 		var item = items[Math.floor(Math.random()*items.length)];
-		$.blockUI({ 
-			message: '<img src="/assets/img/troll' + item + '.gif"/>', 
-			css: { 
-				top:  ($(window).height() - 400) /2 + 'px', 
+		$.blockUI({
+			message: '<img style="max-width: 500px;" src="/assets/img/troll' + item + '.gif"/>',
+			css: {
+				top:  ($(window).height() - 400) /2 + 'px',
 				left: ($(window).width() - 400) /2 + 'px',
 				border: 0
-			} 
+			}
 		});
 	}
 
